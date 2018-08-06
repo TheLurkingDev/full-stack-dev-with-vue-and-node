9aab8355d070a05cfabc0c107e5ce82079f4f98b
TheLurkingDev <thelurkingdev@gmail.com>
2018-08-06 16:59:04 -0400

    Added vuetify and bootstrap.

--- movie_rating_app/src/main.js
+++ movie_rating_app/src/main.js
@@ -1,10 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import BootstrapVue from 'bootstrap-vue';
 
-Vue.config.productionTip = false
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import App from './App';
+import router from './router';
+
+Vue.use(BootstrapVue);
+Vue.use(Vuetify);
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +20,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
-})
+});
